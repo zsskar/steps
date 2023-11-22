82dab363c08790bc770c1f6368653ea1d2d2d5f9
Rashid Ali Khan <rashid.11612612@gmail.com>
2023-11-22 16:06:10 +0530

    project setup is done

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div class="steps">
+      <div class="number">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+
+      <p class="message">message</p>
+
+      <div class="buttons">
+        <button>Previous</button>
+        <button>Next</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
